5bceddc809a3957d1c07ec90e71f27ad3b968192
Vladimir <falinvlad93@rambler.ru>
2023-01-01 13:57:44 +0300

    error

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from "dotenv";
+import AuthRoute from './routes/AuthRoute.js';
 dotenv.config();
 
 
-// mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
 mongoose
-    .connect( 'mongodb+srv://admin:chat@chat.f3sr6wb.mongodb.net/?retryWrites=true&w=majority')
+    .connect( 'mongodb+srv://admin:consultation@consultation.i47rtmp.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB Ok'))
     .catch((err) => console.log(`${err} did not connect`))
 
 // routes
-import AuthRoute from './routes/AuthRoute.js';
 
 // const MONGODB_URI = process.env.MONGODB_URI;
 // const PORT = process.env.PORT;
@@ -24,3 +24,10 @@ app.use(cors());
 
 //Path 
 app.use('/auth', AuthRoute);
+
+app.listen( process.env.PORT || 5000, (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log('Server OK');
+  });
