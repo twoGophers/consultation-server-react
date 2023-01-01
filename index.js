8fe9fc408207a892d4d7f9122e20dc2ff6426f8e
Vladimir <falinvlad93@rambler.ru>
2023-01-01 13:01:05 +0300

    add atlas mongo db

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 // routes
 import AuthRoute from './routes/AuthRoute.js';
 
-const CONNECTION = process.env.MONGODB_CONNECTION;
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
 const app = express();
@@ -15,11 +15,11 @@ app.use(express.json());
 app.use(express.static('avatar'));
 app.use(cors());
 
-//Path 
-app.use('/auth', AuthRoute);
-
 mongoose.set('strictQuery', true);
 mongoose
-    .connect( CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect( MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen( PORT || 5000 , () => console.log(`Listening at Port ${PORT}. Server Ok`)))
     .catch((err) => console.log(`${err} did not connect`))
+
+//Path 
+app.use('/auth', AuthRoute);
