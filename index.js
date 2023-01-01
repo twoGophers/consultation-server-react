c96dc24d47977ecba6abdf26ec1cf95c7d10b25a
Vladimir <falinvlad93@rambler.ru>
2023-01-01 13:25:57 +0300

    relax

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ dotenv.config();
 // routes
 import AuthRoute from './routes/AuthRoute.js';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-const PORT = process.env.PORT;
+// const MONGODB_URI = process.env.MONGODB_URI;
+// const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
@@ -17,7 +17,7 @@ app.use(cors());
 
 mongoose.set('strictQuery', true);
 mongoose
-    .connect( 'mongodb://127.0.0.1:27017/test')
+    .connect( 'mongodb+srv://admin:consultation@consultation.i47rtmp.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen( PORT || 5000 , () => console.log(`Listening at Port ${PORT}. Server Ok`)))
     .catch((err) => console.log(`${err} did not connect`))
 
