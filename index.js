51d8bf88ec762ca9268ab5d7f2ad26137ab5a73f
Vladimir <falinvlad93@rambler.ru>
2023-01-01 13:20:02 +0300

    change url mongoose

--- index.js
+++ index.js
@@ -7,22 +7,19 @@ dotenv.config();
 // routes
 import AuthRoute from './routes/AuthRoute.js';
 
-// const MONGODB_URI = process.env.MONGODB_URI;
-// const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 app.use(express.static('avatar'));
 app.use(cors());
 
-// mongoose.set('strictQuery', true);
-// mongoose
-//     .connect( 'mongodb+srv://admin:consultation@consultation.i47rtmp.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => app.listen( PORT || 5000 , () => console.log(`Listening at Port ${PORT}. Server Ok`)))
-//     .catch((err) => console.log(`${err} did not connect`))
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+mongoose.set('strictQuery', true);
+mongoose
+    .connect( 'mongodb://127.0.0.1:27017/test')
+    .then(() => app.listen( PORT || 5000 , () => console.log(`Listening at Port ${PORT}. Server Ok`)))
+    .catch((err) => console.log(`${err} did not connect`))
 
 //Path 
 app.use('/auth', AuthRoute);
