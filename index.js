a4e1a3a9aac8c0a0b3b9e8a24d0935d58c22db03
Vladimir <falinvlad93@rambler.ru>
2023-01-01 14:00:08 +0300

    fix server mongodb

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ import dotenv from "dotenv";
 import AuthRoute from './routes/AuthRoute.js';
 dotenv.config();
 
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
 
 mongoose.set('strictQuery', true);
 mongoose
-    .connect( 'mongodb+srv://admin:consultation@consultation.i47rtmp.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect( MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB Ok'))
     .catch((err) => console.log(`${err} did not connect`))
 
 // routes
 
-// const MONGODB_URI = process.env.MONGODB_URI;
-// const PORT = process.env.PORT;
+
 
 const app = express();
 app.use(express.json());
@@ -25,7 +26,7 @@ app.use(cors());
 //Path 
 app.use('/auth', AuthRoute);
 
-app.listen( process.env.PORT || 5000, (err) => {
+app.listen( PORT || 5000, (err) => {
     if(err) {
         return console.log(err);
     }
