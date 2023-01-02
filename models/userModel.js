5c0df641d05b63fbbb5bce412c35bdd4fb992a8b
Vladimir <falinvlad93@rambler.ru>
2023-01-02 13:17:18 +0300

    delete email unique

--- models/userModel.js
+++ models/userModel.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema(
       require: true,
       min: 3,
       max: 20,
-      index:{unique: true},
+      // index:{unique: true},
     },
     phone: {
       type: String,
