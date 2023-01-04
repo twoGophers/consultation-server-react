ea7631c7c607c9e9ce9cbc8e4182fa14706cc9f8
Vladimir <falinvlad93@rambler.ru>
2023-01-05 01:33:40 +0300

    post question from home

--- controllers/QuestionsController.js
+++ controllers/QuestionsController.js
@@ -1,22 +1,23 @@
-
 import QuestionHome from '../models/questionsHomeModel.js';
 
 //Register
 export const questionsHome = async (req, res) => {
   try {
+    const panel = new QuestionHome({});
+
     const doc = new QuestionHome ({
-        panel: req.body.panel,
+        panel: panel._id,
         title: req.body.title,
         question: req.body.question,
     });
-
     const question_home = await doc.save();
+
+    console.log(doc._id);
 
     res.status(200).json(question_home);
   } catch(err) {
-    console.log(err);
-    res.status(500).json({
-      message: 'Не удалось отправить',
+    res.status(500 || 404).json({
+      message: 'Не удалось отправить, questionsHome',
     })
   };
 };
